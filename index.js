34417328bd4d333b306f5183f7c4d4c1fa83c4bd
tandrea patil <tandreayoung@gmail.com>
2022-02-24 13:05:12 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+function whisper(string) {
+    return string.toLowerCase();
+  }
+function logShout(string) {
+    console.log(string.toUpperCase());
+  }
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+  }
+function sayHiToHeadphonedRoommate(string){
+    const input = string;
+    if (input === string.toLowerCase()){
+        return 'I can\'t hear you!' ;}
+    if (input === string.toUpperCase()){
+            return 'YES INDEED!' ;}
+    if (input === 'Let\'s have dinner together!'){
+        return 'I would love to!';}
+    }    
+    
+    
+  
+
